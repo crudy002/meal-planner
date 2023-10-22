39fe52356a35b0b935c0e5119825daefd8322c02
Christopher Ruediger <christopher.b.ruediger@gmail.com>
2023-10-22 13:50:34 +0000

    Updated app.css and app.js with beginning of Meal Planner content

--- meal-planner/src/App.js
+++ meal-planner/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import './App.css'; // You can add your own styles in App.css
 
 function App() {
+  // State for meal plans (example data for now)
+  const [mealPlans, setMealPlans] = useState([
+    { id: 1, day: 'Monday', meals: ['Breakfast', 'Lunch', 'Dinner'] }
+  ]);
+
+  // Function to add a new meal plan
+  const addMealPlan = () => {
+    const newDay = prompt('Enter a new day:');
+    if (newDay) {
+      const newMealPlan = {
+        id: mealPlans.length + 1,
+        day: newDay,
+        meals: ['Breakfast', 'Lunch', 'Dinner'],
+      };
+      setMealPlans([...mealPlans, newMealPlan]);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MealPlanner</h1>
       </header>
+
+      <main>
+        <button onClick={addMealPlan}>Add Meal Plan</button>
+
+        {/* Display Meal Plans */}
+        {mealPlans.map((mealPlan) => (
+          <div key={mealPlan.id} className="meal-plan">
+            <h2>{mealPlan.day}</h2>
+            <ul>
+              {mealPlan.meals.map((meal) => (
+                <li key={meal}>{meal}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
